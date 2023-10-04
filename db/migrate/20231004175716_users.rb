4f09ea41ee42edc8d55c93c477892a40cd190d90
ctokoli <matumasie@gmail.com>
2023-10-04 22:35:47 +0000

    Update 20231004175716_users.rb

--- db/migrate/20231004175716_users.rb
+++ db/migrate/20231004175716_users.rb
@@ -1,3 +1,11 @@
 class Users < ActiveRecord::Migration[7.0]
-  def change; end
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :photo
+      t.text :bio
+      t.integer :posts_counter
+      t.timestamps
+    end
+  end
 end
