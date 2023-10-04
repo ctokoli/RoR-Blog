08c4fb48b8540b8552a410b90cfaee2e6df63a89
ctokoli <matumasie@gmail.com>
2023-10-04 22:35:47 +0000

    Update 20231004175702_comments.rb

--- db/migrate/20231004175702_comments.rb
+++ db/migrate/20231004175702_comments.rb
@@ -1,9 +1,9 @@
 # comments migration
 class Comments < ActiveRecord::Migration[7.0]
   def change
-    create_table :commnets do |t|
-      t.references :user_id, foreign_key: { to_table: :users }
-      t.references :post_id, foreign_key: { to_table: :posts }
+    create_table :comments do |t|
+      t.references :user, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
       t.text :text
       t.timestamps
     end
