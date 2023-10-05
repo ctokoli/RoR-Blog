f9d2f95251f9ea9679967d3cbc4b056d0f3437f0
ctokoli <matumasie@gmail.com>
2023-10-05 16:11:26 +0000

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comments < ApplicationRecord
   belongs_to :user
 
   def update_post_comment_count
-    post.update(comments_count: post.comments.count)
+    post.update(comments_counter: post.comments.count)
   end
 end
