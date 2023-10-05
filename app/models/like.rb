44895d8043e08a92fff806161213a3f5dcc23f9b
ctokoli <matumasie@gmail.com>
2023-10-05 16:07:58 +0000

    Add like.rb

---
+++ app/models/like.rb
@@ -0,0 +1,11 @@
+class Likes < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  after_create :update_post_like_count
+  after_destroy :update_post_like_count
+
+  def update_post_like_count
+    post.update(likes_count: post.likes.count)
+  end
+end
