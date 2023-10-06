0ca0e66bf2157cb7cb20f1aa4e13b98b66936ac7
ctokoli <matumasie@gmail.com>
2023-10-06 18:00:30 +0000

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   private
 
   def update_user_post_count
