4d33850e4d45a27f58cd3d7cf6019e47ea8798f4
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,43 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject { Post.new(title: 'Title', author: User.create(name: 'KOJO')) }
-
-  describe 'Post Model title validation tests' do
-    it 'should have title present' do
-      subject.title = 'Title'
-      expect(subject).to be_valid
-    end
-
-    it 'should not have title field empty, if then' do
-      subject.title = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'title should be less than or equal to 250 characters' do
-      subject.title = 'a' * 251
-      expect(subject).to_not be_valid
-
-      subject.title = 'a' * 250
-      expect(subject).to be_valid
-    end
-
-    it 'comments_counter must be an integer otherwise' do
-      subject.comments_counter = 'You are not an integer'
-      expect(subject).to_not be_valid
-    end
-  end
-
-  describe 'Post Model method tests' do
-    it 'should return five recent comments' do
-      post = Post.create(title: 'Title', author: User.create(name: 'KOJO'))
-      expect(post.recent_comments).to eq([])
-    end
-
-    it 'should update comments_counter after create' do
-      post = Post.create(title: 'Title', author: User.create(name: 'KOJO'))
-      result = post.update_user_post_count
-      expect(result).to eq(true)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
