4677fa6fbfbcfe08fbc85ab93aef135a62794031
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # config/routes.rb
+
+# Route for listing all user posts
+get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+
+# Route for showing a specific user post
+get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+# Route for listing all users
+get '/users', to: 'users#index'
+
+# Route for showing a specific user
+get '/users/:id', to: 'users#show', as: 'user'
+
 end
