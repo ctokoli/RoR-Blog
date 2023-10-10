5cc93cdeb175052425952ffa52231905165e3551
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-
-  after_create :update_post_like_count
-  after_destroy :update_post_like_count
-
-  def update_post_like_count
-    post.update(likes_counter: post.likes.count)
-  end
 end
