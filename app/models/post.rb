942699b4102a7ad9980334538e0096e6afa827ee
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-
-  attribute :title, :string
-  attribute :text, :text
-  attribute :comments_counter, :integer, default: 0
-  attribute :likes_counter, :integer, default: 0
-
-  after_create :update_user_post_count
-  after_destroy :update_user_post_count
-
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  def update_user_post_count
-    author.update(posts_counter: author.posts.count)
-  end
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+  belongs_to :author
 end
