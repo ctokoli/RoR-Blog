902922544c55252d1a4af0dcb56fde87a851a32f
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: :user_id
-  belongs_to :post, class_name: 'Post', foreign_key: :post_id
-
-  def update_post_comment_count
-    post.update(comments_counter: post.comments.count)
-  end
+  belongs_to :user
+  belongs_to :post
 end
