423d89a459d1103a262621da596ac68ce9ed3541
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update like_spec.rb

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,16 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  describe 'Like Model Method test' do
-    user = User.create(name: 'Harry')
-    post = Post.create(title: 'Title')
-    like = Like.create(user: user, post: post)
-    like.inspect
-
-    it 'should update likes_counter after create' do
-      like.update_post_like_count
-      result = post.likes_counter
-      expect(result).to eq(0)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
