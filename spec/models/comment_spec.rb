b4742a31757e0548430b41e19d130d8aef20c5e1
ctokoli <matumasie@gmail.com>
2023-10-10 16:37:00 +0000

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,20 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  describe 'Comment Model Method test' do
-    user = User.create(name: 'Harry')
-    post = Post.create(title: 'Title')
-    comment = Comment.create(text: 'Text', author: user, post: post)
-
-    it 'should update comments_counter after create' do
-      comment.update_post_comment_count
-      result = post.comments_counter
-      expect(result).to eq(0)
-    end
-
-    it 'Comment should be text' do
-      result = comment.text
-      expect(result).to eq('Text')
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
