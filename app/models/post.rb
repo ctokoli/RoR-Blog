624f74bdc0798acd811331e954ed0f408a0379be
ctokoli <matumasie@gmail.com>
2023-10-11 11:27:05 +0000

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,13 +14,11 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  
-  
   def update_post_counter
     author&.update(postsCounter: author.posts.count)
   end
