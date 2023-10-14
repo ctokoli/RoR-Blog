7f6d7a2cebfaebf6bf3b64355c3874210f689260
ctokoli <matumasie@gmail.com>
2023-10-14 09:48:52 +0000

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
+
+  def new
+    @user = current_user
+    @post = @user.posts.build
+  end
+
+  def create
+    @user = current_user
+    @post = Post.new(
+      author: @user,
+      title: params[:post][:title],
+      text: params[:post][:text],
+    )
+
+    if @post.save
+      flash.now[:error] = 'Post was successfully created.'
+      redirect_to user_posts_path(@user)
+    else
+      flash.now[:error] = 'Oops, something went wrong'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
