ae09be3604bc504221ba97606e8ded738dee008d
ctokoli <matumasie@gmail.com>
2023-10-17 13:09:16 +0000

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:posts).find(params[:id])
     @posts = @user.posts
   end
 end
