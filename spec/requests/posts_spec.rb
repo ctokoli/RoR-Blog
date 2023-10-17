94140d95fa9e925f84bbd239afba27ca80ed946f
ctokoli <matumasie@gmail.com>
2023-10-17 13:09:16 +0000

    Update posts_spec.rb

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe 'Controllers', type: :request do
       get '/users/1/posts'
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:index)
-      expect(response.body).to include('This page contains a list of Post')
+      expect(response.body).to include('Harry')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success and renders the show template' do
-      get '//users/1/posts/1'
+      get '//users/297/posts/182'
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
-      expect(response.body).to include('More details of a specific post')
+      expect(response.body).to include('Harry')
     end
   end
 end
