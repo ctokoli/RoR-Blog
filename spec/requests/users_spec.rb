3d551409a8b88b8d39e7d4f3b9eea4fe2088c05f
ctokoli <matumasie@gmail.com>
2023-10-17 13:09:16 +0000

    Update users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe 'Controllers', type: :request do
       get '/users'
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:index)
-      expect(response.body).to include('This page contains a list of Users')
+      expect(response.body).to include('Harry')
     end
   end
-
+  
   describe 'GET /show' do
     it 'returns http success and renders the show template' do
       get '/users/1'
       expect(response).to have_http_status(:success)
       expect(response).to render_template(:show)
-      expect(response.body).to include('More details of a specific user')
+      expect(response.body).to include('Harry')
     end
   end
 end
