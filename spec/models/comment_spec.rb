adfa33c3fa12907fe3134993eac1dba86b2ae285
ctokoli <matumasie@gmail.com>
2023-10-17 13:09:16 +0000

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'Comment Model Method test' do
-    user = User.create(name: 'Harry')
+    user = User.create(name: 'Harry', photo: 'https://www.pngegg.com/en/png-wgjix')
     post = Post.create(title: 'Title')
-    comment = Comment.create(text: 'Text', author: user, post: post)
+    comment = Comment.create(text: 'Text', user: user, post: post)
 
     it 'should update comments_counter after create' do
       comment.update_comments_counter
